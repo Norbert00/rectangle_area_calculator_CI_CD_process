4a21bbf89c3c7822a49d5f5ec81767743ca5c290
Norbert00 <norbert.jurek@protonmail.com>
2023-11-15 17:57:13 +0100

    Create bug to test CICD process

--- tests/test_rectangle_calculator.py
+++ tests/test_rectangle_calculator.py
@@ -1,10 +1,12 @@
 import pytest
 from rectangle_calculator.rectangle_calculator import calculate_rectangle_area
 
+
 def test_calculate_rectangle_area():
     assert calculate_rectangle_area(2, 3) == 6
     assert calculate_rectangle_area(5, 5) == 25
-    assert calculate_rectangle_area(1.5, 2.5) == 3.75
+    assert calculate_rectangle_area(1.5, 2.5) == 10  # to fix 3.75
+
 
 def test_invalid_input():
     try:
@@ -22,27 +24,34 @@ def test_invalid_input():
     except ValueError as e:
         assert str(e) == "could not convert string to float: 'hello'"
 
+
 def test_rectangle_area_positive_numbers():
     assert calculate_rectangle_area(5, 10) == 50
+
 
 def test_rectangle_area_negative_numbers():
     with pytest.raises(ValueError):
         calculate_rectangle_area(-5, 10)
 
+
 def test_rectangle_area_one_negative_number():
     with pytest.raises(ValueError):
         calculate_rectangle_area(-5, 5)
+
 
 def test_rectangle_area_zero_width():
     with pytest.raises(ValueError):
         calculate_rectangle_area(0, 10)
 
+
 def test_rectangle_area_zero_length():
     with pytest.raises(ValueError):
         calculate_rectangle_area(5, 0)
 
+
 def test_rectangle_area_large_numbers():
     assert calculate_rectangle_area(1000000, 1000000) == 1000000000000
+
 
 def test_rectangle_area_decimal_numbers():
     assert calculate_rectangle_area(2.5, 3) == 7.5
@@ -56,8 +65,8 @@ def test_rectangle_area_invalid_input():
 def test_rectangle_area_negative_float_input():
     with pytest.raises(ValueError):
         calculate_rectangle_area(-2.5, 3.5)
-        
+
+
 def test_rectangle_area_with_both_negative_numbers():
     with pytest.raises(ValueError):
         calculate_rectangle_area(-5, -5)
-        
